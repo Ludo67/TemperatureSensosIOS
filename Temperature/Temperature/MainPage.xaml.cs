bc7c56f0a17069f71a0d53d39972f2e9fbc8746e
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2021-11-21 02:49:52 -0500

    Add project files.

---
+++ Temperature/Temperature/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+
+namespace Temperature
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Temp();
+        }
+
+        public async void Temp()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://webapprouting.herokuapp.com/");
+            if (response == null || response == "")
+            {
+                await DisplayAlert("Error","Data could not be retrieved.The sensor is either " +
+                     "malfunctioning or is offline." + "Try another time","OK");
+            }
+            var temp = JsonConvert.DeserializeObject<List<Temp>>(response);
+            ListTemp.ItemsSource = temp;
+
+        }
+    }
+}
---
+++ Temperature/Temperature/Temp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Temperature
+{
+    class Temp
+    {
+        public string temperature { get; set; }
+
+        public string humidity { get; set; }
+
+        public string timestamp { get; set; }
+    }
+}
