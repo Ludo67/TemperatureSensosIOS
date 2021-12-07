2cf775b120d57d2f0c30ccf4ae99a17e5f1e52e2
Ludovic <ludomarcoux098@gmail.com>
2021-12-07 15:47:37 -0500

    Added refresh button

--- Temperature/Temperature/App.xaml.cs
+++ Temperature/Temperature/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Temperature
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
